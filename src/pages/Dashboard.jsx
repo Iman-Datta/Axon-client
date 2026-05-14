c335cee0ec8e7a189a696c66c8fff507d89299a4
Iman Datta <dattaiman56@gmail.com>
2026-05-14 12:59:29 +0530

    feat: set up Vite React frontend and basic auth pages

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+function Dashboard() {
+  return (
+    <div
+      style={{
+        padding: "40px",
+      }}
+    >
+      <h1>Welcome to Axon Dashboard</h1>
+
+      <p>User successfully logged in.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/register/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Registration Successful!");
+        console.log(data);
+      } else {
+        alert("Registration Failed");
+        console.log(data);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Server Error");
+    }
+  };
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f4f4f4",
+      }}
+    >
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+          padding: "30px",
+          backgroundColor: "white",
+          borderRadius: "10px",
+          width: "300px",
+        }}
+      >
+        <h2>Register</h2>
+
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
