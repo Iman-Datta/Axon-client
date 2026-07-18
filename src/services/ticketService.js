77794d2867d7d2e87afc3ad1a9daf97f0b801630
Iman Datta <dattaiman56@gmail.com>
2026-07-18 15:26:03 +0530

    updateTicket service created

--- src/services/ticketService.js
+++ src/services/ticketService.js
@@ -10,7 +10,7 @@ export const getMyTickets = async (
   filters = {},
 ) => {
   const query = new URLSearchParams(filters).toString();
-  
+
   const url =
     `${API}/tickets/${workspaceSlug}/${projectSlug}/` +
     (query ? `?${query}` : "");
@@ -50,6 +50,36 @@ export const createTicket = async (
 
   if (!res.ok) {
     throw new Error(data.message || "Failed to create ticket.");
+  }
+
+  return data;
+};
+
+export const updateTicket = async (
+  workspaceSlug,
+  projectSlug,
+  ticket_id,
+  ticketData,
+  dispatch,
+  accessToken,
+) => {
+  const res = await fetchWithAuth(
+    `${API}/tickets/${workspaceSlug}/${projectSlug}/${ticket_id}/update`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(ticketData),
+    },
+    dispatch,
+    accessToken,
+  );
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Failed to update ticket.");
   }
 
   return data;
