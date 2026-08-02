be4cb02edc8c461b9a738846d1f69a6704e31814
Iman Datta <dattaiman56@gmail.com>
2026-08-03 01:57:34 +0530

    update network setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // or "0.0.0.0"
+    port: 5173,
+  },
+});
