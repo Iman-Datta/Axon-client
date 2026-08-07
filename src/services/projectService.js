1474c23f195dc767e6fb49599c941bfa9a985a24
Iman Datta <dattaiman56@gmail.com>
2026-08-07 08:02:41 +0530

    feat(settings): implement project settings pages and leave project flow

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,6 +1,7 @@
 import { fetchWithAuth } from "../utils/fetchWithAuth";
 
 const API = import.meta.env.VITE_API_URL;
+
 export const getMyProjects = async (workspaceSlug, dispatch, accessToken) => {
   const response = await fetchWithAuth(
     `${API}/projects/${workspaceSlug}/my/`,
@@ -90,4 +91,56 @@ export const addMember = async (
   }
 
   return data;
+};
+
+export const updateProject = async (
+  slug,
+  project_slug,
+  projectData,
+  dispatch,
+  accessToken,
+) => {
+  const res = await fetchWithAuth(
+    `${API}/projects/${slug}/${project_slug}/update`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(projectData),
+    },
+    dispatch,
+    accessToken,
+  );
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw data;
+  }
+
+  return data;
+};
+
+export const leaveProject = async (
+  slug,
+  project_slug,
+  dispatch,
+  accessToken,
+) => {
+  const res = await fetchWithAuth(
+    `${API}/projects/${slug}/${project_slug}/leave/`,
+    {
+      method: "DELETE",
+    },
+    dispatch,
+    accessToken,
+  );
+
+  if (!res.ok) {
+    const responseData = await res.json();
+    throw new Error(responseData.message || "Failed to leave project.");
+  }
+
+  return res.status === 204 ? null : await res.json();
 };
